dd6a68d756741a18cd58642b162e6375574a9529
Leonardo Miranda <lmmiranda@pop-os.localdomain>
2022-10-23 13:35:49 +0100

    initial commit

---
+++ src/main/java/com/devsuperior/dscatalogapi/DscatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApiApplication.class, args);
+	}
+
+}
